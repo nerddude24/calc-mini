680d409726f4c239a442e8013b7fd0c8144da365
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-04-27 14:11:35 +0100

    Implement operate function

--- app.js
+++ app.js
@@ -17,3 +17,19 @@ function mul(a, b) {
 function div(a, b) {
 	return a / b;
 }
+
+function operate(a, b, op) {
+	switch (op) {
+		case "+":
+			return add(a, b);
+		case "-":
+			return sub(a, b);
+		case "*":
+			return mul(a, b);
+		case "/":
+			return div(a, b);
+		default:
+			console.error("UNKNOWN OPERATOR: (" + op + ")");
+			return NaN;
+	}
+}

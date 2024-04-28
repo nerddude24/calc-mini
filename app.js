c2654929681c0871fd8503248d16be19a8d583f2
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-04-28 21:22:23 +0100

    Implement working operator and equals buttons
    When an operator button is pressed, such as +, we
    first check if the first number has already been
    stored. If not, we store the number on the display
    and the operator selected, then we clear the
    display. If it has already been stored, we store
    second number and operate on it and display the
    result. And the equals button does the same as if
    the first number is stored and the display is not
    empty.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const displayElement = document.querySelector("#display-txt");
 
-let firstNum;
-let secondNum;
-let operator;
+let firstNum = NaN;
+let secondNum = NaN;
+let operator = "";
 let displayValue = "";
+
+let waitingForSecondNum = false;
 
 function add(a, b) {
 	return a + b;
@@ -22,6 +24,9 @@ function div(a, b) {
 }
 
 function operate(a, b, op) {
+	console.log(a);
+	console.log(b);
+	console.log(op);
 	switch (op) {
 		case "+":
 			return add(a, b);
@@ -37,26 +42,79 @@ function operate(a, b, op) {
 	}
 }
 
+// This clears the display only
 function clearDisplay() {
 	displayValue = "";
 	displayElement.textContent = displayValue;
 }
 
+// This clears everything
+function clearCalculator() {
+	clearDisplay();
+	firstNum = NaN;
+	secondNum = NaN;
+	operator = "";
+}
+
 function populateDisplay(str) {
+	if (displayValue === "NaN") clearDisplay();
 	displayValue += str;
 	displayElement.textContent = displayValue;
 }
 
-const numButtons = document.querySelectorAll(".num-btn");
+// triggers on operator button click
+function processInput(op) {
+	// If the input is empty just return
+	if (displayValue === "") return;
 
+	if (waitingForSecondNum) {
+		calculate();
+		return;
+	}
+
+	console.log(displayValue);
+	firstNum = parseInt(displayValue);
+	console.log(firstNum);
+	operator = op;
+	waitingForSecondNum = true;
+	clearDisplay();
+}
+
+function calculate() {
+	if (!waitingForSecondNum) return;
+
+	secondNum = parseInt(displayValue);
+	const result = operate(firstNum, secondNum, operator);
+	if (result === NaN) {
+		clearCalculator();
+		alert("ERROR: Result is NaN!");
+		return;
+	}
+	displayValue = result.toString();
+	displayElement.textContent = displayValue;
+	operator = "";
+	waitingForSecondNum = false;
+}
+
+const numButtons = document.querySelectorAll(".num-btn");
 numButtons.forEach((element) => {
-	element.addEventListener("click", (event) => {
+	element.addEventListener("click", (_) => {
 		// Add the num that's written inside the button to the display
 		populateDisplay(element.textContent);
 	});
 });
 
+const operationButtons = document.querySelectorAll(".op-btn");
+operationButtons.forEach((element) => {
+	element.addEventListener("click", (_) => {
+		processInput(element.textContent);
+	});
+});
+
 const clearButton = document.querySelector(".ac-btn");
-clearButton.addEventListener("click", clearDisplay);
+clearButton.addEventListener("click", clearCalculator);
+
+const equalsButton = document.querySelector(".eq-btn");
+equalsButton.addEventListener("click", calculate);
 
 clearDisplay();
